d047d1fe84d1617e2a4d6fe67892dce7b1c98bd4
lilong01 <lilong01@fenbi.com>
2019-11-06 14:09:16 +0800

    more

--- processor.py
+++ processor.py
@@ -8,6 +8,7 @@ import sys
 import re
 import os
 import progressbar
+from progressbar import *
 import shutil
 from xml.dom.minidom import Document
 from Stack import *
@@ -16,9 +17,12 @@ from ProcessLineResult import *
 import webbrowser
 
 # 工程中的html css js文件的路径
-HTML_ABS_PATH = os.path.realpath(os.path.abspath(os.path.dirname(sys.argv[0])) + os.path.sep + "XMLDisplay.html");
-CSS_ABS_PATH = os.path.realpath(os.path.abspath(os.path.dirname(sys.argv[0])) + os.path.sep + "XMLDisplay.css");
-JS_ABS_PATH = os.path.realpath(os.path.abspath(os.path.dirname(sys.argv[0])) + os.path.sep + "XMLDisplay.js");
+HTML_ABS_PATH = os.path.realpath(
+    os.path.abspath(os.path.dirname(sys.argv[0])) + os.path.sep + "XMLDisplay.html");
+CSS_ABS_PATH = os.path.realpath(
+    os.path.abspath(os.path.dirname(sys.argv[0])) + os.path.sep + "XMLDisplay.css");
+JS_ABS_PATH = os.path.realpath(
+    os.path.abspath(os.path.dirname(sys.argv[0])) + os.path.sep + "XMLDisplay.js");
 
 # XML node 属性名字：方法名
 XML_NODE_ATTR_METHOD_SIGNATURE = "method"
@@ -62,21 +66,28 @@ doc.appendChild(rootNode)
 # android framework中的包名, 这些包名需要被过滤掉
 AndroidFrameworkPackageNames = ["android\.", "java\."];
 
+
 def getAndroidFrameworkPackageNamesRE():
     strAndroidFrameworkPackageNamesRE = "("
     for i in range(0, len(AndroidFrameworkPackageNames)):
         if i == 0:
-            strAndroidFrameworkPackageNamesRE = strAndroidFrameworkPackageNamesRE + AndroidFrameworkPackageNames[i]
+            strAndroidFrameworkPackageNamesRE = strAndroidFrameworkPackageNamesRE + \
+                                                AndroidFrameworkPackageNames[i]
         else:
-            strAndroidFrameworkPackageNamesRE = strAndroidFrameworkPackageNamesRE + "|" + AndroidFrameworkPackageNames[i]
+            strAndroidFrameworkPackageNamesRE = strAndroidFrameworkPackageNamesRE + "|" + \
+                                                AndroidFrameworkPackageNames[i]
     return strAndroidFrameworkPackageNamesRE + ")";
+
 
 # 用于正则表达式的android framework包名
 AndroidFrameworkRE = getAndroidFrameworkPackageNamesRE()
 
+
 # 解析trace文件
 # 最终输出xml html css js这四个文件到trace同级目录下，然后用浏览器打开html作为最终显示的结果
-def processTrace(strTraceFileAbsPath):
+def processTrace(strTraceFilePath):
+
+    strTraceFileAbsPath = os.path.abspath(strTraceFilePath)
 
     # 输入的trace文件的目录
     strTraceFileDirPath = os.path.split(strTraceFileAbsPath)[0]
@@ -85,7 +96,8 @@ def processTrace(strTraceFileAbsPath):
     strTraceFileName = os.path.splitext(os.path.split(strTraceFileAbsPath)[1])[0];
 
     # 输出的过滤后的方法执行信息的文本文件路径
-    strMethodExecutionInfoOutputAbsPath = os.path.join(strTraceFileDirPath, strTraceFileName + ".txt")
+    strMethodExecutionInfoOutputAbsPath = os.path.join(strTraceFileDirPath,
+                                                       strTraceFileName + ".txt")
 
     # 输出的xml文件的路径
     strXMLOutputAbsPath = os.path.join(strTraceFileDirPath, strTraceFileName + ".xml")
@@ -107,7 +119,9 @@ def processTrace(strTraceFileAbsPath):
     if os.path.exists(strJSOutputAbsPath):
         os.remove(strJSOutputAbsPath)
 
-    bar = progressbar.ProgressBar();
+    widgets = [Timer()]
+
+    bar = progressbar.ProgressBar(widgets=widgets);
 
     commandDmTraceDump = ["dmtracedump", "-o", strTraceFileAbsPath]
     pOpenInstance = subprocess.Popen(commandDmTraceDump, stdout=PIPE, bufsize=1)
@@ -131,16 +145,23 @@ def processTrace(strTraceFileAbsPath):
 
     # (1)针对方法的耗时，进行排序并选取前几个标注为特殊颜色
     # 同时针对方法的耗时，计算占总时间的百分比，并标注在xml中
-    methodEntersSorted = sorted(methodEnters, key=lambda methodExecution:methodExecution.executionTimeMicroSec, reverse=True)
+    methodEntersSorted = sorted(methodEnters,
+                                key=lambda methodExecution: methodExecution.executionTimeMicroSec,
+                                reverse=True)
 
     priority = 0;
     for methodExecutionEnter in methodEntersSorted:
-        nodeForMethodExecutionEnter = doc.getElementsByTagName("_tmp_" + str(methodExecutionEnter.order))[0]
-        nodeForMethodExecutionEnter.setAttribute(XML_NODE_ATTR_METHOD_TIME, str(methodExecutionEnter.executionTimeMicroSec))
-        if priority <= LEAST_PRIORITY_CONCERNED and int(nodeForMethodExecutionEnter.getAttribute(XML_NODE_ATTR_DEPTH)) < 3:
-            nodeForMethodExecutionEnter.setAttribute(XML_NODE_ATTR_METHOD_TIME_PRIORITY, str(priority))
+        nodeForMethodExecutionEnter = \
+        doc.getElementsByTagName("_tmp_" + str(methodExecutionEnter.order))[0]
+        nodeForMethodExecutionEnter.setAttribute(XML_NODE_ATTR_METHOD_TIME,
+                                                 str(methodExecutionEnter.executionTimeMicroSec))
+        if priority <= LEAST_PRIORITY_CONCERNED and int(
+                nodeForMethodExecutionEnter.getAttribute(XML_NODE_ATTR_DEPTH)) < 3:
+            nodeForMethodExecutionEnter.setAttribute(XML_NODE_ATTR_METHOD_TIME_PRIORITY,
+                                                     str(priority))
             priority = priority + 1
-        nodeForMethodExecutionEnter.setAttribute(XML_NODE_ATTR_PERCENTAGE, str(round(100.0 * methodExecutionEnter.executionTimeMicroSec / totalTimeMicroSec)) + "%")
+        nodeForMethodExecutionEnter.setAttribute(XML_NODE_ATTR_PERCENTAGE, str(
+            round(100.0 * methodExecutionEnter.executionTimeMicroSec / totalTimeMicroSec)) + "%")
         nodeForMethodExecutionEnter.tagName = "_" + str(methodExecutionEnter.executionTimeMicroSec)
 
     # (2)处理stack中剩余项的信息，剩余项是有ENTER无EXIT的方法执行，正常情况下总数应该<=1个
@@ -153,7 +174,7 @@ def processTrace(strTraceFileAbsPath):
 
     # (3) xml内容写入磁盘
     with open(strXMLOutputAbsPath, 'w') as f:
-        #　写入xml内容
+        # 　写入xml内容
         strXML = doc.toprettyxml(indent='\t', encoding='utf-8').decode()
         # 用浏览器打开xml的时候需注掉下面两行
         # strXML = re.sub(r"&lt;", "<", strXML)
@@ -181,7 +202,8 @@ def processTrace(strTraceFileAbsPath):
     # 替换掉js里的getXMLString方法的返回值
     with open(strJSOutputAbsPath, "r") as jsFile:
         jsContent = jsFile.read()
-        jsContent = jsContent.replace("XML_STR_PLACE_HOLDER", xmlContent.replace("\"", "\\\"").replace("\n", "\"+\n\""))
+        jsContent = jsContent.replace("XML_STR_PLACE_HOLDER",
+                                      xmlContent.replace("\"", "\\\"").replace("\n", "\"+\n\""))
     with open(strJSOutputAbsPath, "w") as jsFile:
         jsFile.write(jsContent)
 
@@ -189,15 +211,15 @@ def processTrace(strTraceFileAbsPath):
     stack.print()
 
     # 用浏览器打开html
-    webbrowser.open(strHTMLOutputAbsPath)
+    webbrowser.open("file:///" + strHTMLOutputAbsPath)
     pass;
 
 
 # 处理dmtracedump得到的trace文本文件中的一行
 def processLine(order, strLine):
-
     # 如果该行是线程号-线程名的对应，则存入表中
-    isThreadMap = re.match(r"^[0-9]+ ([a-zA-Z0-9-_:/.\u4e00-\u9fa5]+[ :-_])*[a-zA-Z0-9-_:/.\u4e00-\u9fa5]+$", strLine)
+    isThreadMap = re.match(
+        r"^[0-9]+ ([a-zA-Z0-9-_:/.\u4e00-\u9fa5]+[ :-_])*[a-zA-Z0-9-_:/.\u4e00-\u9fa5]+$", strLine)
     splitResult = strLine.split(" ", 1)
     if isThreadMap:
         threadNumber = splitResult[0]
@@ -223,8 +245,8 @@ def processLine(order, strLine):
         try:
             # 创建methodExecution，因为有时候dmtrace.trace中的线程表不全，所以这里try catch一下，让找不到线程名的methodExecution被忽略掉
             methodExecution = MethodExecution(order, threadMap[strMethodThreadNumber],
-                                          strMethodSignature, strMethodClass,
-                                          strExecutionBoundaryAction, strElapsedTimeMicroSec)
+                                              strMethodSignature, strMethodClass,
+                                              strExecutionBoundaryAction, strElapsedTimeMicroSec)
         except BaseException:
             return ProcessLineResult(order, strLine, strElapsedTimeMicroSec)
 
@@ -237,7 +259,8 @@ def processLine(order, strLine):
             # if methodExecution.methodBoundaryAction == MethodExecution.ENTER:
             stack.push(methodExecution)
             node = doc.createElement("_tmp_" + str(methodExecution.order))
-            node.setAttribute(XML_NODE_ATTR_METHOD_SIGNATURE, re.sub(r"^\.+", "", methodExecution.strMethodSignature))
+            node.setAttribute(XML_NODE_ATTR_METHOD_SIGNATURE,
+                              re.sub(r"^\.+", "", methodExecution.strMethodSignature))
             node.setAttribute(XML_NODE_ATTR_DEPTH, "1")
             # 这时xml也是空的，加入首个node
             rootNode.appendChild(node)
@@ -252,17 +275,22 @@ def processLine(order, strLine):
                 methodExecution.counterPartOrder = methodExecutionInStack.order
                 methodExecutionInStack.executionTimeMicroSec = methodExecution.elapsedTimeMicroSec - methodExecutionInStack.elapsedTimeMicroSec
                 # 即将出栈的MethodExecution是方法执行开始时的信息，现在已经执行完了，给他设置上总耗时
-                nodeForMethodExecutionInStack = doc.getElementsByTagName("_tmp_" + str(methodExecution.counterPartOrder))[0]
+                nodeForMethodExecutionInStack = \
+                doc.getElementsByTagName("_tmp_" + str(methodExecution.counterPartOrder))[0]
                 methodEnters.append(methodExecutionInStack)
 
                 # 计算其上级node中所包含的所有方法的总时间
                 if nodeForMethodExecutionInStack.parentNode:
-                    if nodeForMethodExecutionInStack.parentNode.getAttribute(XML_NODE_ATTR_CHILD_METHOD_TIME):
-                        parentNodeChildMethodTime = int(nodeForMethodExecutionInStack.parentNode.getAttribute(XML_NODE_ATTR_CHILD_METHOD_TIME))
+                    if nodeForMethodExecutionInStack.parentNode.getAttribute(
+                            XML_NODE_ATTR_CHILD_METHOD_TIME):
+                        parentNodeChildMethodTime = int(
+                            nodeForMethodExecutionInStack.parentNode.getAttribute(
+                                XML_NODE_ATTR_CHILD_METHOD_TIME))
                     else:
                         parentNodeChildMethodTime = 0
                     parentNodeChildMethodTime = parentNodeChildMethodTime + methodExecutionInStack.executionTimeMicroSec
-                    nodeForMethodExecutionInStack.parentNode.setAttribute(XML_NODE_ATTR_CHILD_METHOD_TIME, str(parentNodeChildMethodTime))
+                    nodeForMethodExecutionInStack.parentNode.setAttribute(
+                        XML_NODE_ATTR_CHILD_METHOD_TIME, str(parentNodeChildMethodTime))
 
                 stack.pop()
             # 如果无法配对，则入栈，并写入xml
@@ -277,17 +305,20 @@ def processLine(order, strLine):
                 nodeDepth = int(parentNodeDepth) + 1
                 node.setAttribute(XML_NODE_ATTR_DEPTH, str(nodeDepth))
                 parentNode.appendChild(node)
-                node.setAttribute(XML_NODE_ATTR_METHOD_SIGNATURE, re.sub(r"^\.+", "", methodExecution.strMethodSignature))
+                node.setAttribute(XML_NODE_ATTR_METHOD_SIGNATURE,
+                                  re.sub(r"^\.+", "", methodExecution.strMethodSignature))
                 # 入栈
                 stack.push(methodExecution)
         return ProcessLineResult(order, strLine, strElapsedTimeMicroSec)
 
+
 # 检测两个MethodExecution是否信息一样，但是动作相反（一个是进入，一个是退出）
-def matchMethodExecution(methodExecution1:MethodExecution, methodExecution2:MethodExecution):
+def matchMethodExecution(methodExecution1: MethodExecution, methodExecution2: MethodExecution):
     return methodExecution1.strMethodThreadName == methodExecution2.strMethodThreadName and methodExecution1.strMethodClass == methodExecution2.strMethodClass and methodExecution1.strMethodSignature == methodExecution2.strMethodSignature and methodExecution1.methodBoundaryAction != methodExecution2.methodBoundaryAction
 
+
 # 检测某条方法执行信息是否要被过滤掉（按线程/方法包名等过滤）
-def shouldBeFiltered(methodExecution:MethodExecution):
+def shouldBeFiltered(methodExecution: MethodExecution):
     shouldBeFiltered = False
     # 非主线程的过滤掉
     if methodExecution.strMethodThreadName != "main":
@@ -295,13 +326,15 @@ def shouldBeFiltered(methodExecution:MethodExecution):
     # framework中的类的包名过滤掉
     elif re.match(r".*" + AndroidFrameworkRE + ".*", methodExecution.strMethodSignature):
         shouldBeFiltered = True
-    return  shouldBeFiltered
+    return shouldBeFiltered
+
 
 # 入口
 # 无参数，用示例的example_dmtrace.txt
 if len(sys.argv) == 1:
     print("WARNING : NO ARGUMENTS, EXAMPLE DMTRACE WILL BE USED")
-    processTrace(os.path.realpath(os.path.abspath(os.path.dirname(sys.argv[0])) + os.path.sep + "example_dmtrace.trace"))
+    processTrace(os.path.realpath(
+        os.path.abspath(os.path.dirname(sys.argv[0])) + os.path.sep + "example_dmtrace.trace"))
     exit(0)
 # 输入一个参数，即trace文件的绝对路径
 elif len(sys.argv) == 2:
